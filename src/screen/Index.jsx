9821bbcf4295ac3dc95c48ea9fdac9bdc5c612a3
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-25 00:19:20 +0700

    add page landing page

---
+++ src/screen/Index.jsx
@@ -0,0 +1,70 @@
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons/faBars';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
+import {useNavigation} from '@react-navigation/native';
+
+const Index = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.nav}>
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <View>
+            <FontAwesomeIcon icon={faBars} color="white" />
+          </View>
+          <View>
+            <FontAwesomeIcon icon={faMagnifyingGlass} color="white" />
+          </View>
+        </TouchableOpacity>
+        <View />
+      </View>
+      <View style={{alignItems: 'center', marginTop: 20}}>
+        <TextInput
+          style={styles.input}
+          placeholder="Search event..."
+          placeholderTextColor="white"
+        />
+      </View>
+
+      <View>
+        <Text>this content</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#0E8388',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  nav: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 5,
+  },
+  img: {
+    width: 50,
+    height: 50,
+  },
+  input: {
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 20,
+    width: '90%',
+    height: 55,
+    paddingLeft: 30,
+  },
+});
+export default Index;
