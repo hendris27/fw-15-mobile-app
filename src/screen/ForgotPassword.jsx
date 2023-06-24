7e7244ca6c84bff027944fbc4a53a27f667acbb3
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-25 00:19:05 +0700

    add page forgot password

---
+++ src/screen/ForgotPassword.jsx
@@ -0,0 +1,66 @@
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
+import React from 'react';
+
+const ForgotPassword = () => {
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.textTitle}>
+        <Text style={{fontSize: 24, fontWeight: '600', color: 'black'}}>
+          Forgot Password
+        </Text>
+        <View style={styles.textSubTitle}>
+          <Text style={{color: 'black'}}>
+            You’ll get mail soon on your email{' '}
+          </Text>
+        </View>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          placeholder="email"
+          placeholderTextColor="gray"
+        />
+      </View>
+
+      <View
+        style={{
+          paddingLeft: 10,
+          paddingRight: 10,
+          marginTop: 30,
+          borderRadius: 20,
+        }}>
+        <Button style={styles.btn} title="Confirm" />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {height: '100%'},
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    color: 'black',
+    paddingLeft: 15,
+    paddingBottom: 10,
+    paddingTop: 10,
+    maxHeight: 50,
+  },
+  textTitle: {
+    color: 'black',
+    marginTop: 30,
+    padding: 20,
+    flexDirection: 'coloumn',
+    rowGap: 20,
+  },
+  textSubTitle: {
+    flexDirection: 'row',
+    columnGap: 5,
+  },
+});
+
+export default ForgotPassword;
