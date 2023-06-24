d7a5e4d5f1b062f62c02ec862cfa048082461f8f
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-25 00:18:16 +0700

    add pages

--- App.jsx
+++ App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Home from './src/screen/Home';
 import Login from './src/screen/Login';
 import Register from './src/screen/Register';
+import Index from './src/screen/Index';
+import ForgotPassword from './src/screen/ForgotPassword';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -11,9 +13,11 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Index" component={Index} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
       </Stack.Navigator>
     </NavigationContainer>
   );
