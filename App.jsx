3bdd6d4034ce41eb99e073087034bde61d94f096
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-28 01:02:07 +0700

    add redux store

--- App.jsx
+++ App.jsx
@@ -1,25 +1,14 @@
-import {View, Text} from 'react-native';
 import React from 'react';
-import Home from './src/screen/Home';
-import Login from './src/screen/Login';
-import Register from './src/screen/Register';
-import Index from './src/screen/Index';
-import ForgotPassword from './src/screen/ForgotPassword';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer} from '@react-navigation/native';
+import Main from './src/screen/Main';
 
-const Stack = createNativeStackNavigator();
+import store from './src/redux/store';
+import {Provider} from 'react-redux';
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Index" component={Index} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 };
 
