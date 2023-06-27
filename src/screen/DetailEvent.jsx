9eea00588d225408b1fbb8baba9c47add715e553
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-27 17:12:16 +0700

    add detail event

---
+++ src/screen/DetailEvent.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const DetailEvent = () => {
+  return (
+    <View>
+      <Text>DetailEvent</Text>
+    </View>
+  );
+};
+
+export default DetailEvent;
