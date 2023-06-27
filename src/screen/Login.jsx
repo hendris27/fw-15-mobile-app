bdf2ad4d0c313485b160355a515c3e85f96e75f9
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-27 17:12:54 +0700

    fix login

--- src/screen/Login.jsx
+++ src/screen/Login.jsx
@@ -4,13 +4,9 @@ import {
   StyleSheet,
   TouchableOpacity,
   TextInput,
-  SafeAreaView,
-  Pressable,
 } from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-// import Icon from 'react-native-vector-icons/FontAwesome';
-// import {FaRocket} from 'react-icons/fa';
 
 const Register = () => {
   const navigation = useNavigation();
@@ -18,75 +14,49 @@ const Register = () => {
   return (
     <View style={styles.wrapper}>
       <View style={styles.textTitle}>
-        <Text style={{fontSize: 24, fontWeight: '600', color: 'black'}}>
-          Login
-        </Text>
+        <Text style={styles.title}>Login</Text>
         <View style={styles.textSubTitle}>
-          <Text style={{color: 'black'}}>Hi, Welcome back to Urticket! </Text>
+          <Text style={styles.textColor}>Hi, Welcome back to Urticket! </Text>
         </View>
       </View>
-      {/* <View>
-        <FaRocket size={30} color="#900" />
-      </View> */}
       <View />
-      <View>
-        <SafeAreaView>
+      <View style={styles.parentInput}>
+        <View>
           <TextInput
             style={styles.input}
             placeholder="email"
             placeholderTextColor="gray"
           />
-        </SafeAreaView>
-        <SafeAreaView>
+        </View>
+        <View>
           <TextInput
             secureTextEntry
             style={styles.input}
             placeholder="password"
             placeholderTextColor="gray"
           />
-        </SafeAreaView>
+        </View>
       </View>
       <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
-        <Text
-          style={{
-            color: 'black',
-            fontWeight: '600',
-            textAlign: 'right',
-            paddingRight: 15,
-          }}>
-          Forgot Password?
-        </Text>
+        <Text style={styles.textForgotPassword}>Forgot Password?</Text>
       </TouchableOpacity>
-      <View
-        style={{
-          paddingLeft: 10,
-          paddingRight: 10,
-          marginTop: 30,
-          borderRadius: 20,
-        }}>
-        <Pressable style={styles.btn}>
-          <Text style={{color: 'white', fontSize: 18}}>Login</Text>
-        </Pressable>
-      </View>
-      <View
-        style={{
-          flexBasis: 'coloumn',
-          rowGap: 10,
-          marginTop: 40,
-          alignItems: 'center',
-        }}>
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
+
+      <View style={styles.loginWith}>
         <View>
-          <Text style={{color: 'black'}}>Login With</Text>
+          <Text style={styles.textColor}>Login With</Text>
         </View>
-        <View style={{flexDirection: 'row'}}>
+        <View style={styles.iconloginWith}>
           <View>
-            <Text style={{color: 'black'}}>1</Text>
+            <Text>1</Text>
           </View>
           <View>
-            <Text style={{color: 'black'}}>2</Text>
+            <Text>2</Text>
           </View>
           <View>
-            <Text style={{color: 'black'}}>3</Text>
+            <Text>3</Text>
           </View>
         </View>
       </View>
@@ -95,32 +65,70 @@ const Register = () => {
 };
 
 const styles = StyleSheet.create({
-  wrapper: {height: '100%'},
+  wrapper: {
+    height: '100%',
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
   btn: {
+    width: '100%',
+    marginTop: 30,
     backgroundColor: '#0E8388',
-    padding: 15,
+    height: 55,
     alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 20,
   },
-
+  btnText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  parentInput: {
+    flexDirection: 'column',
+    rowGap: 20,
+  },
   input: {
     height: 55,
-    margin: 10,
     borderWidth: 1,
-    padding: 20,
+    padding: 10,
     borderRadius: 10,
     color: 'black',
+    fontSize: 18,
+    marginTop: 20,
   },
   textTitle: {
     color: 'black',
     marginTop: 30,
-    padding: 20,
     flexDirection: 'coloumn',
     rowGap: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'black',
   },
   textSubTitle: {
     flexDirection: 'row',
     columnGap: 5,
+  },
+  textColor: {
+    color: 'black',
+  },
+  textForgotPassword: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'black',
+    textAlign: 'right',
+    paddingRight: 10,
+  },
+  loginWith: {
+    flexBasis: 'coloumn',
+    rowGap: 10,
+    marginTop: 40,
+    alignItems: 'center',
+  },
+  iconloginWith: {
+    flexDirection: 'row',
   },
 });
 
