099e8b670078ef7bc00eb8a45f1f69495083795c
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-30 14:09:46 +0700

    initial commit

---
+++ src/redux/actions/auth.js
@@ -0,0 +1,22 @@
+import {createAsyncThunk} from '@reduxjs/toolkit';
+import http from '../../helpers/https';
+
+export const asyncLogin = createAsyncThunk(
+  'asyncLogin',
+  async (payload, {rejectWithValue}) => {
+    try {
+      const form = new URLSearchParams();
+      form.append('email', payload.email);
+      form.append('password', payload.password);
+      const {data} = await http().post('/auth/login', form.toString());
+      return data.results.token;
+    } catch (err) {
+      const message = err?.response?.data?.message;
+      if (message) {
+        return rejectWithValue(message);
+      } else {
+        return rejectWithValue(err.message);
+      }
+    }
+  },
+);
