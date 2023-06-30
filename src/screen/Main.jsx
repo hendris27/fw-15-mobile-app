f9008641584c727158a426f4361b0708d5d3d902
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-06-30 14:11:08 +0700

    add drawer

--- src/screen/Main.jsx
+++ src/screen/Main.jsx
@@ -3,31 +3,38 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Home from './Home';
 import Login from './Login';
-import Signup from './Signup';
+import Profile from './Profile';
 import Register from './Register';
 import Index from './Index';
 import ForgotPassword from './ForgotPassword';
+import {useSelector} from 'react-redux';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 const AuthStack = createNativeStackNavigator();
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const Main = () => {
+  const token = useSelector(state => state.auth.token);
   return (
     <NavigationContainer>
-      <AuthStack.Navigator screenOptions={{headerShown: false}}>
-        <AuthStack.Screen name="Register" component={Register} />
-        <AuthStack.Screen name="Login" component={Login} />
-        <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
-      </AuthStack.Navigator>
+      {!token && (
+        <AuthStack.Navigator screenOptions={{headerShown: false}}>
+          <AuthStack.Screen name="Register" component={Register} />
+          <AuthStack.Screen name="Login" component={Login} />
+          <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+        </AuthStack.Navigator>
+      )}
 
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Index" component={Index} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <Stack.Screen name="Signup" component={Signup} />
-      </Stack.Navigator> */}
+      {token && (
+        <>
+          <Drawer.Navigator>
+            <Drawer.Screen name="Index" component={Index} />
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Profile" component={Profile} />
+          </Drawer.Navigator>
+        </>
+      )}
     </NavigationContainer>
   );
 };
