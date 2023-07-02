0e8cc17062c13be274238f9a996cb00223178c4c
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-02 18:47:28 +0700

    initial commit

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -20,3 +20,69 @@ export const asyncLogin = createAsyncThunk(
     }
   },
 );
+export const asyncRegister = createAsyncThunk(
+  'asyncRegister',
+  async (payload, {rejectWithValue}) => {
+    try {
+      const form = new URLSearchParams();
+      console.log(payload);
+      form.append('fullName', payload.fullName);
+      form.append('email', payload.email);
+      form.append('password', payload.password);
+      form.append('confirmPassword', payload.confirmPassword);
+
+      const {data} = await http().post('/auth/register', form.toString());
+      return data.message;
+    } catch (err) {
+      // const messageForgot = err?.response?.data?.results[0].msg;
+      const message = err?.response?.data?.message;
+      if (message) {
+        return rejectWithValue(message);
+      } else {
+        return rejectWithValue(err.message);
+      }
+    }
+  },
+);
+export const asyncForgotPassword = createAsyncThunk(
+  'asyncForgotPassword',
+  async (payload, {rejectWithValue}) => {
+    try {
+      const form = new URLSearchParams();
+      console.log(payload);
+      form.append('email', payload.email);
+
+      const {data} = await http().post('/auth/forgotPassword', form.toString());
+      return data.message;
+    } catch (err) {
+      const message = err?.response?.data?.message;
+      if (message) {
+        return rejectWithValue(message);
+      } else {
+        return rejectWithValue(err.message);
+      }
+    }
+  },
+);
+export const asyncResetPassword = createAsyncThunk(
+  'asyncResetPassword',
+  async (payload, {rejectWithValue}) => {
+    try {
+      const form = new URLSearchParams();
+      form.append('code', payload.code);
+      form.append('email', payload.email);
+      form.append('password', payload.password);
+      form.append('confirmPassword', payload.confirmPassword);
+
+      const {data} = await http().post('/auth/resetPassword', form.toString());
+      return data.message;
+    } catch (err) {
+      const message = err?.response?.data?.message;
+      if (message) {
+        return rejectWithValue(message);
+      } else {
+        return rejectWithValue(err.message);
+      }
+    }
+  },
+);
