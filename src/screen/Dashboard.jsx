c0f57ead342a124b2701ce4e0eac498b23da6d9d
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-04 09:37:14 +0700

    integrasi get categories

--- src/screen/Dashboard.jsx
+++ src/screen/Dashboard.jsx
@@ -1,37 +1,73 @@
+import {View, Text, Image, StyleSheet} from 'react-native';
 import {
-  View,
+  ScrollView,
   TextInput,
   TouchableOpacity,
-  ScrollView,
-  Text,
-  Image,
-} from 'react-native';
+} from 'react-native-gesture-handler';
 import * as React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons/faMagnifyingGlass';
 import globalStyles from '../assets/css/globalStyles';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
-import {useNavigation} from '@react-navigation/native';
+import {useFocusEffect} from '@react-navigation/native';
 import http from '../helpers/https';
 import moment from 'moment';
+import {useSelector} from 'react-redux';
 
-const Index = () => {
-  const [events, setEvents] = React.useState([]);
+const Home = ({navigation}) => {
   const defaultimg = require('../assets/img/default-profile.jpg');
-  async function getDataEvent() {
-    try {
-      const {data} = await http().get('/event?limit=10');
-      setEvents(data.results);
-    } catch (error) {
-      const message = error?.response?.data?.message;
-      if (message) {
-        console.log(message);
+  const token = useSelector(state => state.auth.token);
+  const [events, setEvents] = React.useState([]);
+  const [eventCategories, setEventCategories] = React.useState([]);
+  const [eventCategoriesData, setEventCategoriesData] = React.useState([]);
+  const [search, setSearch] = React.useState('');
+
+  useFocusEffect(
+    React.useCallback(() => {
+      async function getDataEvent() {
+        try {
+          const {data} = await http().get('/event?limit=10');
+          setEvents(data.results);
+        } catch (error) {
+          const message = error?.response?.data?.message;
+          if (message) {
+            console.log(message);
+          }
+        }
       }
+      getDataEvent();
+    }, []),
+  );
+
+  async function getEventByCategory(name) {
+    const {data} = await http(token).get('/event', {params: {category: name}});
+    setEventCategoriesData(data.results);
+  }
+
+  async function getEventByName(search) {
+    const eventdata = await http(token).get('/events?limit=20');
+    const {data} = await http(token).get('/event', {
+      params: {search: search},
+    });
+    if (search === '') {
+      setEvents(eventdata.data.results);
+    } else {
+      setEvents(data.results);
     }
   }
-  getDataEvent();
-  const navigation = useNavigation();
+  useFocusEffect(
+    React.useCallback(() => {
+      async function getEventCategories() {
+        let {data} = await http(token).get('/category?limit=7');
+        setEventCategories(data.results);
+      }
+
+      getEventCategories();
+      getEventByCategory();
+    }, []),
+  );
+
   return (
     <View style={globalStyles.wrapperDasboard}>
       <View>
@@ -52,6 +88,9 @@ const Index = () => {
         <TextInput
           style={globalStyles.inputComponentsDashboard}
           placeholder="Search event..."
+          onChangeText={ev => setSearch(ev)}
+          onSubmitEditing={() => getEventByName(search)}
+          onBlur={() => getEventByName()}
         />
         <FontAwesomeIcon icon={faMagnifyingGlass} color="white" />
       </View>
@@ -68,16 +107,7 @@ const Index = () => {
                 onPress={() => navigation.navigate('DetailEvent')}
                 style={globalStyles.boxEvent}>
                 {!event.picture && (
-                  <Image
-                    style={globalStyles.img}
-                    source={
-                      event.picture(defaultimg)
-                        ? event.picture
-                        : `${import.meta.env.BACKEND_URL}/uploads/${
-                            event.picture
-                          }`
-                    }
-                  />
+                  <Image style={globalStyles.img} source={defaultimg} />
                 )}
                 {event.picture && (
                   <Image
@@ -106,104 +136,24 @@ const Index = () => {
               </TouchableOpacity>
             );
           })}
-          <TouchableOpacity
-            onPress={() => navigation.navigate('DetailEvent')}
-            style={globalStyles.boxEvent}>
-            <Image
-              source={require('../assets/img/event2.jpg')}
-              style={globalStyles.img}
-            />
-
-            <View style={globalStyles.wrapperTitleTextHome}>
-              <Text style={globalStyles.textTitleDetail}>
-                Wed, 10 Nov, 4:00 PM
-              </Text>
-              <Text style={globalStyles.textTitleMain}>
-                Sights & Sounds Exhibition
-              </Text>
-              <TouchableOpacity
-                style={globalStyles.btnArrowRight}
-                onPress={() => navigation.navigate('Events')}>
-                <View>
-                  <FeatherIcon name="arrow-right" size={30} color="white" />
-                </View>
-              </TouchableOpacity>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('DetailEvent')}
-            style={globalStyles.boxEvent}>
-            <Image
-              source={require('../assets/img/event3.jpg')}
-              style={globalStyles.img}
-            />
-
-            <View style={globalStyles.wrapperTitleTextHome}>
-              <Text style={globalStyles.textTitleDetail}>
-                Wed, 10 Nov, 4:00 PM
-              </Text>
-              <Text style={globalStyles.textTitleMain}>
-                Sights & Sounds Exhibition
-              </Text>
-              <TouchableOpacity
-                style={globalStyles.btnArrowRight}
-                onPress={() => navigation.navigate('Events')}>
-                <View>
-                  <FeatherIcon name="arrow-right" size={30} color="white" />
-                </View>
-              </TouchableOpacity>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('DetailEvent')}
-            style={globalStyles.boxEvent}>
-            <Image
-              source={require('../assets/img/event2.jpg')}
-              style={globalStyles.img}
-            />
-
-            <View style={globalStyles.wrapperTitleTextHome}>
-              <Text style={globalStyles.textTitleDetail}>
-                Wed, 10 Nov, 4:00 PM
-              </Text>
-              <Text style={globalStyles.textTitleMain}>
-                Sights & Sounds Exhibition
-              </Text>
-              <TouchableOpacity
-                style={globalStyles.btnArrowRight}
-                onPress={() => navigation.navigate('Events')}>
-                <View>
-                  <FeatherIcon name="arrow-right" size={30} color="white" />
-                </View>
-              </TouchableOpacity>
-            </View>
-          </TouchableOpacity>
         </ScrollView>
         <View>
           <Text style={globalStyles.textTitleDiscover}>Discover</Text>
         </View>
         <ScrollView style={globalStyles.wrapperBox} horizontal={true}>
-          <View style={globalStyles.wrapperBoxNew}>
-            <TouchableOpacity
-              style={globalStyles.wrapperBoxDiscover}
-              onPress={() => navigation.navigate('MyBooking')}>
-              <View style={globalStyles.iconDiscover}>
-                <EntypoIcon name="location-pin" size={30} color="black" />
+          {eventCategories.map(eventCat => {
+            return (
+              <View key={eventCat.id}>
+                <TouchableOpacity
+                  style={style.boxEventCategories}
+                  onPress={() => getEventByCategory(eventCat.name)}>
+                  <Text style={{color: 'black'}}>{eventCat.name}</Text>
+                </TouchableOpacity>
               </View>
-              <Text style={globalStyles.textDiscover}>Location</Text>
-            </TouchableOpacity>
-          </View>
-          <TouchableOpacity
-            onPress={() => navigation.navigate('MyWishlist')}
-            style={globalStyles.wrapperBoxNew}>
-            <View style={globalStyles.wrapperBoxDiscover}>
-              <View style={globalStyles.iconDiscover}>
-                <FeatherIcon name="music" size={30} color="black" />
-              </View>
-              <Text style={globalStyles.textDiscover}>Music</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity
+            );
+          })}
+
+          {/* <TouchableOpacity
             onPress={() => navigation.navigate('ManageEvent')}
             style={globalStyles.wrapperBoxNew}>
             <View style={globalStyles.wrapperBoxDiscover}>
@@ -212,7 +162,7 @@ const Index = () => {
               </View>
               <Text style={globalStyles.textDiscover}>Technologi</Text>
             </View>
-          </TouchableOpacity>
+          </TouchableOpacity> */}
         </ScrollView>
         <View style={globalStyles.containerUpcoming}>
           <Text style={globalStyles.containerTextUpcoming}>Upcoming</Text>
@@ -442,5 +392,19 @@ const Index = () => {
     </View>
   );
 };
+const style = StyleSheet.create({
+  boxEventCategories: {
+    width: 120,
+    height: 50,
+    borderRadius: 30,
+    elevation: 5,
+    shadowColor: '#52006A',
+    backgroundColor: 'gray',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+});
 
-export default Index;
+export default Home;
