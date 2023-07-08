eb9c215316dce51faa2ecc6ea891b69578182390
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-07-08 08:15:08 +0700

    feat:icon

--- ios/ZonaTicketTests/ZonaTicketTests.m
+++ ios/ZonaTicketTests/ZonaTicketTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface testappTests : XCTestCase
+@interface ZonaTicketTests : XCTestCase
 
 @end
 
-@implementation testappTests
+@implementation ZonaTicketTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
